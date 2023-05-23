8691acb1bd431f7ccefeb3df9fb8b400a57e5f67
myroman-wuLtCL <myroman@protonmail.com>
2023-05-22 22:10:22 -0400

    updated js

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,35 @@ var latitude= "39.952583";
 var longitude= "-75.165222";
 var searchURL= "https://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+"&appid="+APIkey;
 var searchURL_2= `api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIkey}`
-var cityLocation;
+var cityName= document.querySelector("#result-name");
+var temp = document.querySelector("#temp");
+var humidity = document.querySelector("#humidity");
+var wind = document.querySelector("#wind");
+var day1Date = document.querySelector("#day1Date");
+var day2Date = document.querySelector("#day2Date");
+var day3Date = document.querySelector("#day3Date");
+var day4Date = document.querySelector("#day4Date");
+var day5Date = document.querySelector("#day5Date");
+var day1Temp = document.querySelector("#day1Temp");
+var day2Temp = document.querySelector("#day2Temp");
+var day3Temp = document.querySelector("#day3Temp");
+var day4Temp = document.querySelector("#day4Temp");
+var day5Temp = document.querySelector("#day5Temp");
+var day1Humidity = document.querySelector("day1Humidity");
+var day2Humidity = document.querySelector("day2Humidity");
+var day3Humidity = document.querySelector("day3Humidity");
+var day4Humidity = document.querySelector("day4Humidity");
+var day5Humidity = document.querySelector("day5Humidity");
+var day1Wind = document.querySelector("#day1Wind");
+var day2Wind = document.querySelector("#day2Wind");
+var day3Wind = document.querySelector("#day3Wind");
+var day4Wind = document.querySelector("#day4Wind");
+var day5Wind = document.querySelector("#day5Wind");
 
 
-function makeRequest() {
+function makeRequest(city) {
     // make our API request 
-    fetch("")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "9b8b36e29d3934ff62f1a31ee39ae4cc")
         .then(response => {
             console.log("Response:", response);
             return response.json();
